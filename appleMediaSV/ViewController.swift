98a76954f4fc51887abf5cc54d44b20a76a38c26
Xianxian Chen <xianxianchen@ac.c4q.nyc>
2019-02-24 09:51:31 -0500

    first commit

---
+++ appleMediaSV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appleMediaSV
+//
+//  Created by C4Q on 2/24/19.
+//  Copyright © 2019 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
